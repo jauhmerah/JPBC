94125dea74a62cfc84ba3c59749c8aa21a8602f6
jauhmerah <jauhmerah@gmail.com>
2022-08-01 21:05:47 +0800

    Photon Engine In-progress : need to fixed cube

---
+++ Assets/Scripts/Setting/ConnectToServer.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+namespace JPBC
+{
+    public class ConnectToServer : MonoBehaviourPunCallbacks
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+
+        public override void OnConnectedToMaster()
+        {
+            //base.OnConnectedToMaster();
+            PhotonNetwork.JoinLobby();
+        }
+
+        public override void OnJoinedLobby()
+        {
+            //base.OnJoinedLobby();
+            SceneManager.LoadScene("Lobby");
+        }
+    }
+}
+
